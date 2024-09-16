608e10539e1ed5318d1a8f8896833c6eaad482a0
Bethlehem Nigussie <bethlehemnigussie19@gmail.com>
2024-09-16 07:29:42 -0700

    first_commit

---
+++ Netflix-Clone/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='app'>
+        <h1>hello</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
